f0c2199a90e75049bdfe3a6e190e1961ef39a258
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-11 16:47:26 +0200

    Created index and show pages

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+end
