dfe06452e96922884444f881d4e5f8a102f5f2d1
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-11 17:49:12 +0200

    Add create and delete features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,28 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def create
+  def new
     @article = Article.new
   end
 
-  def update
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
 
+  def update
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
